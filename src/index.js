45c1d83ef51c6810b5ade675bff9b9bc06cae31d
Happy Enderman <git@happyenderman.com>
2024-11-13 15:45:51 +0100

    feat(workflow): add workflow to scrape strings

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import puppeteer from 'puppeteer';
+import fs from 'fs/promises';
+
+const browser = await puppeteer.launch();
+const page = await browser.newPage();
+
+await page.goto('https://canary.discord.com/login');
+const stringsUnformatted = await page.evaluate(() => {
+    let wreq;
+    webpackChunkdiscord_app.push([[Symbol()], {}, (r) => (wreq = r)]);
+    const chunks = Object.keys(wreq.m)
+        .filter((key) => wreq.m[key].toString().includes('let{createLoader:'))
+        .map(
+            (e) =>
+                wreq.m[e].toString().match(/("|')en-US("|'):\(\)=>.+?\(("|')\d+.+\.then\(.+bind\(.,(?<chunkEnUS>\d+)\)/)
+                    ?.groups?.chunkEnUS,
+        );
+    return chunks.map((chunkId) => wreq(chunkId).default);
+});
+const apiUrl = 'https://intl-stuff.wavedev.lol/api/parse';
+const req = await fetch(apiUrl, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'User-Agent': 'ScraperBot/1.0',
+    },
+    body: JSON.stringify(stringsUnformatted),
+});
+const strings = await req.json();
+await fs.writeFile('./data/strings.json', JSON.stringify(strings, null, 4), 'utf-8');
+await browser.close();
